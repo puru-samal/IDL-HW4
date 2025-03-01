c0934f4aabde528f678d21a047d943f1f9633397
Puru Samal <puruboii@gmail.com>
2025-03-01 18:28:30 -0500

    TA version ready for HW4P1 ablations ...

--- download_data.py
+++
@@ -1,8 +0,0 @@
-import gdown
-data_link = "1-0e9Gnl4nm6wbIuE_Yxl2wRZI8yGxHm6"
-solution_link = "10W49bC2qoHjltNI_4PDqoArZzdgZHBfy"
-url = lambda link: f"https://drive.google.com/uc?id={link}"
-gdown.download(url(data_link), "hw4_data.tar.gz")
-gdown.download(url(solution_link), "hw4p2_sol.json")
-
-
